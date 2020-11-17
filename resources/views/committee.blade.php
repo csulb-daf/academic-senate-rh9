8570a4429ee6bb97582616b9b9fa68d5f3bde3df
Amir <Amir.Zabeti@csulb.edu>
2020-11-17 12:35:47 -0800

    Committee Management page, template layout

--- resources/views/committee.blade.php
+++ resources/views/committee.blade.php
@@ -1,1 +1,45 @@
-<h1>Hello World</h1>
+@push('head')
+	<link rel="stylesheet" href="{{asset('css/datatables.min.css')}}" type="text/css">
+	
+	<script src="{{asset('js/datatables.min.js')}}" defer></script>
+	
+@endpush
+
+@extends('layouts.app')
+@section('content')
+<table id="committeeTable" class="display">
+	<thead>
+		<tr>
+			<th>Column 1</th>
+			<th>Column 2</th>
+			<th>Column 3</th>
+		</tr>
+	</thead>
+
+	<tr>
+		<td>John</td>
+		<td>Doe</td>
+		<td>john@example.com</td>
+	<tr>
+	
+	<tr>
+		<td>Mary</td>
+		<td>Moe</td>
+		<td>mary@example.com</td>
+	</tr>
+
+	<tr>
+		<td>July</td>
+		<td>Dooley</td>
+		<td>july@example.com</td>
+	</tr>
+</table>
+@endsection
+
+@push('scripts')
+<script>
+$(document).ready(function() {
+	$('#committeeTable').DataTable();
+});
+</script>
+@endpush
