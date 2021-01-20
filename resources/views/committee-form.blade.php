1b8669c59e178f8280a6a5e4a9cfa0e33ef80774
Amir <Amir.Zabeti@csulb.edu>
2021-01-19 21:48:08 -0800

    committee form

--- resources/views/committee-form.blade.php
+++ resources/views/committee-form.blade.php
@@ -16,22 +16,28 @@
 <form method="POST" id="commForm" action="/committee/add">
 	@csrf
 
-	<div class="input-group">
-		<label for="commName" style="margin-top: 1em;">Committee Name:</label>
-		<input class="form-control" type="text" name="commName" id="commName" value="{{ old('commName') }}" >
+	<div class="form-group row">
+		<label for="commName" class="col-sm-2 col-form-label">Committee Name:</label>
+		<div class="col-sm-10">
+			<input class="form-control" type="text" name="commName" id="commName" value="{{ old('commName') }}" >
+		</div>
 	</div>
 
-	<div class="input-group">
-		<label for="meetTime" style="margin-top: 1em;">Meeting Time and Location:</label>
-		<input class="form-control" type="text" name="meetTime" id="meetTime" value="{{ old('meetTime') }}" >
+	<div class="form-group row">
+		<label for="meetTime" class="col-sm-2 col-form-label">Meeting Time and Location:</label>
+		<div class="col-sm-10">
+			<input class="form-control" type="text" name="meetTime" id="meetTime" value="{{ old('meetTime') }}" >
+		</div>
 	</div>
 	
-	<div class="input-group">
-		<label for="notes" style="margin-top: 1em;">Notes:</label>
-		<textarea class="form-control" name="notes" id="notes" >{{ old('notes') }}</textarea>
+	<div class="form-group row">
+		<label for="notes" class="col-sm-2 col-form-label">Notes:</label>
+		<div class="col-sm-10">
+			<textarea class="form-control" name="notes" id="notes" >{{ old('notes') }}</textarea>
+		</div>
 	</div>
 
-	<div class="form-group ">
+	<div class="form-group">
 		<button class="btn btn-primary mt-3" type="submit">Create Committee</button>
 	</div>
 
