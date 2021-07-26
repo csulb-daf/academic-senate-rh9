53ecbf694baced984a5261e2ee1da551944efc4b
Amir <Amir.Zabeti@csulb.edu>
2021-07-26 10:38:18 -0700

    include students in search

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -51,7 +51,7 @@ class Controller extends BaseController {
 		// configure ldap params
 		ldap_set_option($connection, LDAP_OPT_PROTOCOL_VERSION, 3);
 		ldap_set_option($connection, LDAP_OPT_REFERRALS, 0);
-		$filter = "(&(objectClass=user)(|(extensionattribute11=Active)(extensionattribute11=Unassigned)(extensionattribute11=Leave)(extensionattribute11=Leave with Pay)(extensionattribute11=Short Work Break))(|(sn=$request->q*)(givenname=$request->q*)(displayname=$request->q*)))";
+		$filter = "(&(objectClass=user)(|(extensionattribute11=Active)(extensionattribute11=Unassigned)(extensionattribute11=Leave)(extensionattribute11=Leave with Pay)(extensionattribute11=Short Work Break)(employeetype=Student))(|(sn=$request->q*)(givenname=$request->q*)(displayname=$request->q*)))";
 		$attributes = array('employeeid', 'givenname', 'sn', 'department', 'division', 'mail', 'telephonenumber', 'employeetype');
 		$entry = ldap_search($connection, $search_basedn, $filter,  $attributes);
 		
