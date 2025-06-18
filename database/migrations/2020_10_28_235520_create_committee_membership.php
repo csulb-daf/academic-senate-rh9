16c6f4cf5c073574081da1071e7d14765c58b763
Shift <shift@laravelshift.com>
2025-06-18 23:13:05 +0000

    Adopt anonymous migrations

--- database/migrations/2020_10_28_235520_create_committee_membership.php
+++ database/migrations/2020_10_28_235520_create_committee_membership.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommitteeMembership extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -46,4 +46,4 @@ class CreateCommitteeMembership extends Migration
     {
         Schema::dropIfExists('committee_membership');
     }
-}
+};
